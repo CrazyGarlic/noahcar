b51d79b3067f978bf409a3d34dae48840c4d2e26
Go Ducks78 <go.ducks78@gmail.com>
2025-01-15 21:23:16 -0800

    using picam1

--- autorc/profiles/default.py
+++ autorc/profiles/default.py
@@ -1,7 +1,7 @@
 import os
 from autorc.config import config
 from autorc.vehicle import Vehicle
-from autorc.nodes.camera import CVWebCam  # , PGWebCam
+from autorc.nodes.camera import PiCamera1  # , PGWebCam
 from autorc.nodes.engine import Engine
 from autorc.nodes.web import WebController
 from autorc.nodes.recorder import SimpleRecorder
@@ -9,7 +9,7 @@ from autorc.nodes.pilot import KerasSteeringPilot
 
 
 vehicle = Vehicle()
-vehicle.add_node(CVWebCam, size=(320, 240), jpeg_size=(160, 160),
+vehicle.add_node(PiCamera1, size=(320, 240), jpeg_size=(160, 160),
                  numpy_size=(160, 160))
 vehicle.add_node(WebController)
 vehicle.add_node(Engine)
